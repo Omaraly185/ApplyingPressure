d31c9429830b12573e323d494c618831e0ff8697
omaraly185 <omaraly185@gmail.com>
2023-08-02 14:50:14 -0400

    phone fix

--- src/Pages/Book-Now/Calendar/myCalendar.js
+++ src/Pages/Book-Now/Calendar/myCalendar.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './myCalendar.css';
-import Sidepanel from './SidePanel/sidepanel';
-import moment from 'moment';
-import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import React, { useState, useEffect } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./myCalendar.css";
+import Sidepanel from "./SidePanel/sidepanel";
+import moment from "moment";
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = (props) => {
   const [showPanel, setShowPanel] = useState(false);
-  const [selectedDate, setSelectedDate] = useState('');
-  const [selectedTime, setSelectedTime] = useState('');
+  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedTime, setSelectedTime] = useState("");
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true); // Add loading state
 
@@ -22,12 +22,12 @@ const MyCalendar = (props) => {
 
   const fetchEvents = async () => {
     try {
-      const response = await fetch('http://localhost:5000/events');
+      const response = await fetch("http://localhost:5000/events");
       const events = await response.json();
       setEvents(events);
       setLoading(false); // Update loading state
     } catch (error) {
-      console.error('Error fetching events:', error);
+      console.error("Error fetching events:", error);
       setLoading(false); // Update loading state even if there is an error
     }
   };
@@ -35,24 +35,24 @@ const MyCalendar = (props) => {
   const formatDate = (start) => {
     const dateString = start;
     const date = new Date(dateString);
-    const formattedDate = date.toLocaleDateString('en-US', {
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
+    const formattedDate = date.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
     });
     return formattedDate;
   };
 
   const handleSelectSlot = (slotInfo) => {
     const { start, action } = slotInfo;
-    if (action === 'click') {
+    if (action === "click" || action === "select") {
       if (start < new Date()) {
         toast.error("You can't select a date before today!");
         return;
       }
 
       setSelectedDate(formatDate(start));
-      setSelectedTime('');
+      setSelectedTime("");
       setShowPanel(true);
     }
   };
@@ -63,7 +63,7 @@ const MyCalendar = (props) => {
     const selectedTimeString = `${date} ${time}`;
     const eventDate = new Date(selectedTimeString);
     const newEvent = {
-      title: 'Test Event',
+      title: "Test Event",
       start: eventDate,
       end: eventDate,
     };
@@ -71,7 +71,7 @@ const MyCalendar = (props) => {
   };
 
   if (loading) {
-    return <div style={{ color: 'white' }}>Loading...</div>; // Display a loading message or spinner while events are being fetched
+    return <div style={{ color: "white" }}>Loading...</div>; // Display a loading message or spinner while events are being fetched
   }
 
   return (
@@ -86,7 +86,7 @@ const MyCalendar = (props) => {
         endAccessor="end"
         step={60}
         timeslots={1}
-        views={['month', 'agenda']}
+        views={["month", "agenda"]}
       />
       <Sidepanel
         showPanel={showPanel}
