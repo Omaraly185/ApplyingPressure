9132b7a8a90777a2f081f0249b935c21f680c233
omaraly185 <omaraly185@gmail.com>
2023-10-10 12:53:33 -0400

    removed stuff for memory usage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import Header from '../../Component/Header';
-import './home.css';
-import HomeForm from './HomeForm';
-import NetflixReel from './NetflixReel';
-import AboutUs from './AboutUs/AboutUs';
+import React from "react";
+import Header from "../../Component/Header";
+import "./home.css";
+import HomeForm from "./HomeForm";
+import AboutUs from "./AboutUs/AboutUs";
 function Home() {
   return (
     <div
-      style={{ backgroundColor: 'black' }}
+      style={{ backgroundColor: "black" }}
       className="fluid myCustomHeight fullPage"
     >
       <Header />
       <HomeForm />
       <div></div>
-      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{' '}
-      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{' '}
-      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{' '}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />{" "}
       <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
       <br /> <br />
       <AboutUs />
-      <br /> <br />
-      <NetflixReel />
     </div>
   );
 }
